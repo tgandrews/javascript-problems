3e826f32a67f0c651134cafc15a1b70681aa198b
Tom Andrews <git@tomandrews.co.uk>
2015-01-16 17:04:23 +0200

    Add tests to check for additional dates and bad data

--- test/q3-five-sunday-spec.js
+++ test/q3-five-sunday-spec.js
@@ -9,4 +9,16 @@ describe('Five Sundays', function () {
     var result = fiveSundays(input);
     should(result).equal(2);
   });
+
+  it('returns 2 months with five sundays for December 2013 - May 2014', function () {
+    var input = 'December 2013 May 2014';
+    var result = fiveSundays(input);
+    should(result).equal(2);
+  });
+
+  it('returns blank string when start date is after end date', function () {
+    var input = 'May 2014 December 2013';
+    var result = fiveSundays(input);
+    should(result).equal('');
+  });
 });
