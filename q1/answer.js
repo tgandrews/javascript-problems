b6ba86f139f10c0b7a683b32cb6e7d2a807613a0
Tom Andrews <git@tomandrews.co.uk>
2015-01-16 13:05:59 +0200

    Extract line execution of first arg to a library function

--- q1/answer.js
+++ q1/answer.js
@@ -1,16 +1,7 @@
-var fs = require('fs'),
-  readline = require('readline');
-
-// First arg - 0: node, 1: this file, 2: first arg
-var filePath = process.argv[2];
-
-var reader = readline.createInterface({
-  input: fs.createReadStream(filePath),
-  terminal: false
-});
+var lineExecutor = require('../lib/line-executor');
 
 var lineReverser = function (line) {
   console.log(line.split(' ').reverse().join(' '));
 };
 
-reader.on('line', lineReverser);
+lineExecutor(lineReverser);
